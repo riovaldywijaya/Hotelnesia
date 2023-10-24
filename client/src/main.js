38cf02666da7af73f552d2bf88887a278da3de0f
Riovaldy Wijaya <riovaldy1@gmail.com>
2023-10-24 18:56:38 +0700

    set up app

---
+++ client/src/main.js
@@ -0,0 +1,21 @@
+// import './assets/main.css'
+
+import { createApp, markRaw } from 'vue';
+import { createPinia } from 'pinia';
+
+import App from './App.vue';
+import router from './router';
+
+const app = createApp(App);
+
+const pinia = createPinia();
+
+pinia.use((options) => {
+  options.store.$router = markRaw(router);
+});
+
+app.use(pinia);
+
+app.use(router);
+
+app.mount('#app');
